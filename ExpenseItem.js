484d04147e1019df0f45fc89006b9c6471a3a182
Kiranchavan05 <125879292+Kiranchavan05@users.noreply.github.com>
2023-04-03 17:25:13 +0530

    react 1st expense file

---
+++ ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <h2>Expense Item!</h2>
+        <div>
+            <h2>Food Rs 10</h2>
+            <h2>Petrol Rs 100</h2>
+            <h2>Movies Rs 200</h2>
+        </div>
+    </div>
+        )
+}
+
+export default ExpenseItem;
