678ed6203136d48af35ae6f2147e44b118c05fee
Kiranchavan05 <125879292+Kiranchavan05@users.noreply.github.com>
2023-04-08 13:33:33 +0530

    advance react task3

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
+import NewExpense from './components/NewExpenses/NewExpense';
+import Expenses from "./components/Expenses/Expenses";
+//import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+const App=() => {
   const expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
       amount: "100",
-      date: new Date(2023, 5, 12),
+      date: new Date(2023, 1, 12),
     },
-    { id: "e2", title: "New TV", amount: "500", date: new Date(2023, 6, 11) },
+    { id: "e2", title: "New TV", amount: "500", date: new Date(2023, 2, 11) },
     {
       id: "e3",
       title: "Car Insurance",
@@ -25,31 +28,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
