27f9b3fe7a56517988a566c056c273a8fb1f17e0
Kiranchavan05 <125879292+Kiranchavan05@users.noreply.github.com>
2023-04-08 11:42:54 +0530

    advance react task2

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,17 +1,33 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem=(props)=> {
+
+  const [title, setTitle]=useState(props.title);
+  console.log('ExpenseItem evaluated by React')
   const clickhandler=()=>{
-    console.log('clicked!!!!')
+
+    setTitle('updated!')
+    //title='updated!';
+    console.log(title)
+  }
+
+  const [amount,setAmount]=useState(props.amount);
+
+  const handler=()=>{
+    setAmount('100')
+    //title='updated!';
+    console.log(amount)
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+        <button onClick={handler}>Change Expense</button>
       </div>
       <button onClick={clickhandler}>Delete</button>
     </Card>
