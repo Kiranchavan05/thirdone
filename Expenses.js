60a8aa3a5b3d26c89f8c08fb0867d6b7afccee0f
Kiranchavan05 <125879292+Kiranchavan05@users.noreply.github.com>
2023-04-10 13:57:10 +0530

    advance react task 12

--- Expenses.js
+++ Expenses.js
@@ -14,20 +14,27 @@ const Expense = (props) => {
   const filteredExpenses=props.items.filter(expense=>{
     return expense.date.getFullYear().toString() === filteredYear;
   })
+
+let expensesContent=<p>No Expense Found</p>;
+  if(filteredExpenses.length>0){
+    expensesContent=filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
